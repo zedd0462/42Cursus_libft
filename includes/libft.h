0ac09a47831a2d171515ecbc3621259fd1d3addb
Abdelhamid Fadel <fadelabdelhamid1@gmail.com>
2023-09-23 17:06:24 +0100

    added ft_lstnew

--- includes/libft.h
+++ includes/libft.h
@@ -6,7 +6,7 @@
 /*   By: afadel <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 19:15:43 by afadel            #+#    #+#             */
-/*   Updated: 2023/09/08 06:09:44 by afadel           ###   ########.fr       */
+/*   Updated: 2023/09/23 16:26:46 by afadel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,5 +49,13 @@ void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
+
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}					t_list;
+
+t_list	*ft_lstnew(void *content);
 
 #endif
